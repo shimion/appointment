7fdb6e311a86db5049333d7f850d8f2c7f744230
shimion <shimion_b@yahoo.com>
2015-09-08 23:32:36 +0600

    First update

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+//add_action('init', 'postme');
+
+	function get_appointment_availablity_by_date_and_attendence1(){
+			echo count(array('1'));
+		}
+//add_action('init', 'get_appointment_availablity_by_date_and_attendence1');
